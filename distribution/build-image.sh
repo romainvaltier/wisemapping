d88d90c83887b41341939f4b0fff113cfb85d20a
Romain Valtier <romain@valtier.fr>
2023-03-26 22:57:21 +0200

    Update build-image.sh

--- distribution/build-image.sh
+++ distribution/build-image.sh
@@ -1,7 +1,7 @@
- #!/bin/sh
+#!/bin/sh
 
 set -o
 set -u
 
-mvn -f pom.xml clean package
-docker build --platform linux/amd64 -t wisemapping/wisemapping:latest -f distribution/Dockerfile wise-webapp/target/
+mvn -f ../pom.xml clean package
+docker build --platform linux/amd64 -t wisemapping/wisemapping:latest -f ./Dockerfile ../wise-webapp/target/
